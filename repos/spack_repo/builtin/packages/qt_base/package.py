bdcfe909063def08895c9cbb870f3aa0d933bb3d
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:52:24 +0100

    reduce: 2 line(s) across 2 regions: repos/spack_repo/builtin/packages/qt_base/package.py [4:5]; repos/spack_repo/builtin/packages/qt_base/package.py [0:1]

--- repos/spack_repo/builtin/packages/qt_base/package.py
+++ repos/spack_repo/builtin/packages/qt_base/package.py
@@ -1,8 +1,6 @@
-import sys
 from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 class QtPackage(CMakePackage):
     def get_list_url(qualname):
-        _list_url = "https://github.com/qt/{}/tags"
         return _list_url.format(qualname.lower())
 class QtBase(QtPackage):
     """Qt Base (Core, Gui, Widgets, Network, ...)"""

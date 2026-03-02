988b0ac2bc7a1290ba652d04666e7904acbcc07c
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:52:28 +0100

    reduce: 5 line(s) across 5 regions: repos/spack_repo/builtin/build_systems/cuda.py [5:6]; repos/spack_repo/builtin/build_systems/cuda.py [0:1]; repos/spack_repo/builtin/packages/kokkos/package.py [3:4]; repos/spack_repo/builtin/build_systems/gnu.py [0:1]; repos/spack_repo/builtin/build_systems/_checks.py [0:1]

--- repos/spack_repo/builtin/build_systems/cuda.py
+++ repos/spack_repo/builtin/build_systems/cuda.py
@@ -1,6 +1,4 @@
-from typing import Iterable, List
 from spack.package import PackageBase, any_combination_of, conflicts, depends_on, variant, when
 class CudaPackage(PackageBase):
     """Auxiliary class which contains CUDA variant, dependencies and conflicts
     """
-    # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html#gpu-feature-list

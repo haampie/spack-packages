9be9200aa972b6085dca9216b4e45c7403dd8be8
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:53:07 +0100

    reduce: 1 line(s) across 1 regions: repos/spack_repo/builtin/packages/m4/package.py [4:5]

--- repos/spack_repo/builtin/packages/m4/package.py
+++ repos/spack_repo/builtin/packages/m4/package.py
@@ -2,7 +2,6 @@ from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.gnu import GNUMirrorPackage
 from spack.package import *
 class M4(AutotoolsPackage, GNUMirrorPackage):
-    version("1.4.21", sha256="38ae59f7a30bf9c108193cc5c25fbb06014f21e230c7ede2eff614f7b7c37ed8")
     version("1.4.20", sha256="6ac4fc31ce440debe63987c2ebbf9d7b6634e67a7c3279257dc7361de8bdb3ef")
     depends_on("c", type="build")  # generated
     executables = ["^g?m4$"]

1361b462a98c94326fba1924c047eae57f52d7f5
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:46:05 +0100

    reduce: 4 line(s) across 1 regions: repos/spack_repo/builtin/packages/paraview/package.py [28:32]

--- repos/spack_repo/builtin/packages/paraview/package.py
+++ repos/spack_repo/builtin/packages/paraview/package.py
@@ -26,10 +26,6 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     variant(
         "build_edition",
         default="canonical",
-        multi=False,
-        values=("canonical", "catalyst_rendering", "catalyst", "rendering", "core"),
-        description="Build editions include only certain modules. "
-        "Editions are listed in decreasing order of size.",
     )
     with when("@6:"):
             depends_on("qt-tools+assistant")

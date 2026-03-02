2670fbae31dd519b3f96b410cf02c6efe1c022e0
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:46:23 +0100

    reduce: 9 line(s) across 1 regions: repos/spack_repo/builtin/build_systems/autotools.py [16:25]

--- repos/spack_repo/builtin/build_systems/autotools.py
+++ repos/spack_repo/builtin/build_systems/autotools.py
@@ -14,12 +14,3 @@ from ._checks import ensure_build_dependencies_or_raise, execute_build_time_test
 class AutotoolsPackage(PackageBase):
     """Specialized class for packages built using GNU Autotools."""
     #: This attribute is used in UI queries that need to know the build
-    #: system base class
-class AutotoolsBuilder(BuilderWithDefaults):
-    """The autotools builder encodes the default way of installing software built
-    with autotools. It has four phases that can be overridden, if need be:
-        1. :py:meth:`~.AutotoolsBuilder.autoreconf`
-        2. :py:meth:`~.AutotoolsBuilder.configure`
-        3. :py:meth:`~.AutotoolsBuilder.build`
-    """
-    #: Phases of a GNU Autotools package

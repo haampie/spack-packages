3342f459ae0633ff20e2ec7651dd0f0ebe7e45e7
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:54:02 +0100

    reduce: delete 3 file(s) in repos/spack_repo/builtin/packages/adios2/package.py, repos/spack_repo/builtin/packages/bison/parallel.patch, repos/spack_repo/builtin/packages/boost/package.py

--- repos/spack_repo/builtin/packages/adios2/package.py
+++
@@ -1,6 +0,0 @@
-from spack_repo.builtin.build_systems.cmake import CMakeBuilder, CMakePackage
-from spack_repo.builtin.build_systems.cuda import CudaPackage
-from spack_repo.builtin.build_systems.rocm import ROCmPackage
-class Adios2(CMakePackage, CudaPackage, ROCmPackage):
-    """The Adaptable Input Output System version 2,
-    developed in the Exascale Computing Program"""

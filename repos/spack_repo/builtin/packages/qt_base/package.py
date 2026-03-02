95aa9698a8b52a32ac3a728fcec2a914b2a6caa9
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:45:49 +0100

    reduce: 9 line(s) across 1 regions: repos/spack_repo/builtin/packages/qt_base/package.py [9:18]

--- repos/spack_repo/builtin/packages/qt_base/package.py
+++ repos/spack_repo/builtin/packages/qt_base/package.py
@@ -7,15 +7,6 @@ MACOS_VERSION = macos_version() if sys.platform == "darwin" else None
 class QtPackage(CMakePackage):
     """Base package for Qt6 components"""
     homepage = "https://www.qt.io"
-    @staticmethod
-    def get_url(qualname):
-        _url = "https://github.com/qt/{}/archive/refs/tags/v6.2.3.tar.gz"
-        return _url.format(qualname.lower())
-    @staticmethod
-    def get_git(qualname):
-        _git = "https://github.com/qt/{}.git"
-        return _git.format(qualname.lower())
-    @staticmethod
     def get_list_url(qualname):
         _list_url = "https://github.com/qt/{}/tags"
         return _list_url.format(qualname.lower())

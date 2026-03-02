dda4f5a6803834c1d4977d81ebdc9c904d7aa75a
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:49:54 +0100

    reduce: 3 line(s) across 1 regions: repos/spack_repo/builtin/packages/openfoam/package.py [15:18]

--- repos/spack_repo/builtin/packages/openfoam/package.py
+++ repos/spack_repo/builtin/packages/openfoam/package.py
@@ -13,9 +13,6 @@ class Openfoam(Package):
     variant(
         "paraview", default=False, description="Build paraview plugins and runtime post-processing"
     )
-    variant("vtk", default=False, description="With VTK runTimePostProcessing")
-    # not know which OpenFOAM added support for 5.x and conservatively
-    # use 2312 in the check.
     depends_on("flex@:2.6.1,2.6.4:")
     # Require scotch with ptscotch - corresponds to standard OpenFOAM setup
     # 1706 ok with newer paraview but avoid pv-5.2, pv-5.3 readers

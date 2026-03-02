a66d001e0a9c5adf3b8c17d67d4d07a2ecee79d4
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:49:36 +0100

    reduce: 11 line(s) across 1 regions: repos/spack_repo/builtin/build_systems/_checks.py [0:11]

--- repos/spack_repo/builtin/build_systems/_checks.py
+++ repos/spack_repo/builtin/build_systems/_checks.py
@@ -1,14 +1,3 @@
-from typing import List
-from spack.package import (
-    Builder,
-    Spec,
-)
-def ensure_build_dependencies_or_raise(spec: Spec, dependencies: List[str], error_msg: str):
-    """Ensure that some build dependencies are present in the concrete spec.
-          RuntimeError: when the required build dependencies are not found
-    """
-def execute_build_time_tests(builder: Builder):
-    """Execute the build-time tests prescribed by builder.
     Args:
         builder: builder prescribing the test callbacks. The name of the callbacks is
             stored as a list of strings in the ``build_time_test_callbacks`` attribute.

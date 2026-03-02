2976db527dc515c94de799b128ab68bf337d0983
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:53:53 +0100

    reduce: delete 1 file(s) in repos/spack_repo/builtin/packages/kokkos/package.py

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++
@@ -1,5 +0,0 @@
-from spack_repo.builtin.build_systems.cmake import CMakePackage
-from spack_repo.builtin.build_systems.cuda import CudaPackage
-from spack_repo.builtin.build_systems.rocm import ROCmPackage
-class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
-    homepage = "https://github.com/kokkos/kokkos"

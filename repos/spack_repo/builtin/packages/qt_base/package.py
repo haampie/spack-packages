127f40c9903776a87ec625c977680b45bf427cb9
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:49:09 +0100

    reduce: 6 line(s) across 6 regions: repos/spack_repo/builtin/packages/qt_base/package.py [8:9]; repos/spack_repo/builtin/packages/qt_base/package.py [7:8]; repos/spack_repo/builtin/packages/qt_base/package.py [5:6]; repos/spack_repo/builtin/packages/qt_base/package.py [4:5]; repos/spack_repo/builtin/packages/qt_base/package.py [2:3]; repos/spack_repo/builtin/packages/qt_base/package.py [0:1]

--- repos/spack_repo/builtin/packages/qt_base/package.py
+++ repos/spack_repo/builtin/packages/qt_base/package.py
@@ -1,12 +1,6 @@
-import shutil
 import sys
-import tempfile
 from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
-from spack.package import *
-MACOS_VERSION = macos_version() if sys.platform == "darwin" else None
 class QtPackage(CMakePackage):
-    """Base package for Qt6 components"""
-    homepage = "https://www.qt.io"
     def get_list_url(qualname):
         _list_url = "https://github.com/qt/{}/tags"
         return _list_url.format(qualname.lower())

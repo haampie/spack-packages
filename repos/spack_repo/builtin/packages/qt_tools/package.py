1ad94fdc14531d58cfd720454a2ec2f8ddef6336
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:48:37 +0100

    reduce: 9 line(s) across 9 regions: repos/spack_repo/builtin/packages/bison/package.py [6:7]; repos/spack_repo/builtin/packages/bison/package.py [1:2]; repos/spack_repo/builtin/packages/bison/package.py [0:1]; repos/spack_repo/builtin/packages/qt_tools/package.py [16:17]; repos/spack_repo/builtin/packages/qt_tools/package.py [15:16]; repos/spack_repo/builtin/packages/qt_tools/package.py [13:14]; repos/spack_repo/builtin/packages/qt_tools/package.py [8:9]; repos/spack_repo/builtin/packages/qt_tools/package.py [3:4]; repos/spack_repo/builtin/packages/qt_tools/package.py [4:5]

--- repos/spack_repo/builtin/packages/qt_tools/package.py
+++ repos/spack_repo/builtin/packages/qt_tools/package.py
@@ -1,20 +1,14 @@
 from spack_repo.builtin.packages.qt_base.package import QtBase, QtPackage
 from spack.package import *
 class QtTools(QtPackage):
-    list_url = QtPackage.get_list_url(__qualname__)
-    # src/assistant/qlitehtml is a submodule that is not in the git archive
     variant(
         "assistant",
         default=False,
-        description="Qt Assistant for viewing on-line documentation in Qt help file format.",
     )
     variant(
         "designer",
         default=False,
-        description="Qt Widgets Designer for designing and building GUIs with Qt Widgets.",
     )
-    # use of relative path in https://github.com/qt/qttools/blob/6.8.2/.gitmodules
-    conflicts("+assistant", when="@6.8.2", msg="Incorrect git submodule prevents +assistant")
     depends_on("c")
     depends_on("cxx")
     for _v in QtBase.versions:

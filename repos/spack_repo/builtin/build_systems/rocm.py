e2f02a4de7aff4e706f89faa0854e554fb9928a9
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:48:57 +0100

    reduce: 10 line(s) across 10 regions: repos/spack_repo/builtin/packages/paraview/package.py [10:11]; repos/spack_repo/builtin/packages/paraview/package.py [6:7]; repos/spack_repo/builtin/packages/paraview/package.py [0:1]; repos/spack_repo/builtin/build_systems/rocm.py [14:15]; repos/spack_repo/builtin/build_systems/rocm.py [12:13]; repos/spack_repo/builtin/build_systems/rocm.py [11:12]; repos/spack_repo/builtin/build_systems/rocm.py [7:8]; repos/spack_repo/builtin/build_systems/rocm.py [6:7]; repos/spack_repo/builtin/build_systems/rocm.py [5:6]; repos/spack_repo/builtin/build_systems/rocm.py [4:5]

--- repos/spack_repo/builtin/build_systems/rocm.py
+++ repos/spack_repo/builtin/build_systems/rocm.py
@@ -2,14 +2,7 @@ import os
 from spack.package import (
     EnvironmentModifications,
     PackageBase,
-    any_combination_of,
-    conflicts,
-    depends_on,
-    variant,
 )
 class ROCmPackage(PackageBase):
     """Auxiliary class which contains ROCm variant, dependencies and conflicts
-    and is meant to unify and facilitate its usage. Closely mimics CudaPackage.
-    Maintainers: dtaller
     """
-    # https://llvm.org/docs/AMDGPUUsage.html
--- repos/spack_repo/builtin/packages/paraview/package.py
+++ repos/spack_repo/builtin/packages/paraview/package.py
@@ -1,15 +1,15 @@
-from subprocess import Popen
 from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
 from spack.package import *
 class Paraview(CMakePackage, CudaPackage, ROCmPackage):
-    variant("cdi", default=False, description="Enable CDI support")
     variant(
         "build_edition",
         default="canonical",
-        multi=False,
+        values=("canonical", "catalyst_rendering", "catalyst", "rendering", "core"),
         description="Build editions include only certain modules. "
+        "Editions are listed in decreasing order of size.",
     )
     with when("@6:"):
             depends_on("qt-tools+assistant")
+            depends_on("qt-5compat")

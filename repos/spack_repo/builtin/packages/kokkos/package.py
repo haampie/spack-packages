07f93f0c6a5cab5d05fe6d52dcc696b19c777c55
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-02 15:51:31 +0100

    reduce: 2 line(s) across 1 regions: repos/spack_repo/builtin/packages/kokkos/package.py [5:7]

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -3,6 +3,4 @@ from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
 from spack.package import *
 class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
-    """Kokkos implements a programming model in C++ for writing performance
-    portable applications targeting all major HPC platforms."""
     homepage = "https://github.com/kokkos/kokkos"

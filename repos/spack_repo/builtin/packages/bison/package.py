aea93ba5b79815387c0c50340e043718153813d0
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:52:37 +0100

    reduce: 6 lines across 1 regions

--- repos/spack_repo/builtin/packages/bison/package.py
+++ repos/spack_repo/builtin/packages/bison/package.py
@@ -4,12 +4,6 @@ from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.gnu import GNUMirrorPackage
 from spack.package import *
 class Bison(AutotoolsPackage, GNUMirrorPackage):
-    """Bison is a general-purpose parser generator that converts
-    an annotated context-free grammar into a deterministic LR or
-    generalized LR (GLR) parser employing LALR(1) parser tables."""
-    homepage = "https://www.gnu.org/software/bison/"
-    gnu_mirror_path = "bison/bison-3.6.4.tar.gz"
-    tags = ["build-tools"]
     executables = ["^bison$"]
     version("3.7.5", sha256="151cb5f12716e3fe93a27a317cd44878329659f275b342779bfaef4a526bbf70")
     variant("color", default=False, description="Enable experimental colored output", when="@3.4:")

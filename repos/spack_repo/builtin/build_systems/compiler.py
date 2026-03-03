ba4fd832cb86b3b4a434bdc0e54ff7290cba86c9
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:54:30 +0100

    reduce: 13 lines across 13 regions

--- repos/spack_repo/builtin/build_systems/compiler.py
+++ repos/spack_repo/builtin/build_systems/compiler.py
@@ -4,15 +4,9 @@ from spack.package import (
     PackageBase,
 )
 class CompilerPackage(PackageBase):
-    """A Package mixin for all common logic for packages that implement compilers"""
-    # TODO: how do these play nicely with other tags
     tags: Sequence[str] = ["compiler"]
-    #: Optional suffix regexes for searching for this type of compiler.
-    #: Flag used to get verbose output
     verbose_flags: str = "-v"
-    #: Flag to activate OpenMP support
     @property
     def cc(self) -> Optional[str]:
-        assert self.spec.concrete, "cannot retrieve C compiler, spec is not concrete"
         if self.spec.external:
             return self.spec.extra_attributes.get("compilers", {}).get("c", None)
--- repos/spack_repo/builtin/packages/bison/package.py
+++ repos/spack_repo/builtin/packages/bison/package.py
@@ -1,20 +1,13 @@
-import re
-import sys
 from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 from spack_repo.builtin.build_systems.gnu import GNUMirrorPackage
 from spack.package import *
 class Bison(AutotoolsPackage, GNUMirrorPackage):
-    executables = ["^bison$"]
     version("3.7.5", sha256="151cb5f12716e3fe93a27a317cd44878329659f275b342779bfaef4a526bbf70")
-    variant("color", default=False, description="Enable experimental colored output", when="@3.4:")
-    # https://lists.gnu.org/archive/html/bug-bison/2019-08/msg00008.html
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
     conflicts(
         "%oneapi",
         msg=(
             "bison is likely miscompiled by oneapi compilers, "
-            "see https://github.com/spack/spack/issues/37172"
         ),
     )
-    build_directory = "spack-build"

6bc9c62d76a5cb221c57de82824d2652c473cb4a
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:40:23 +0100

    reduce: 108 lines across 2 regions

--- repos/spack_repo/builtin/packages/gcc/package.py
+++ repos/spack_repo/builtin/packages/gcc/package.py
@@ -62,47 +62,5 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage, CompilerPackage):
     variant("mold", default=False, description="Use mold as the linker by default", when="@12:")
     variant(
         "piclibs", default=False, description="Build PIC versions of libgfortran.a and libstdc++.a"
-    )
-    variant("strip", default=False, description="Strip executables to reduce installation size")
-    variant("nvptx", default=False, description="Target nvptx offloading to NVIDIA GPUs")
-    variant("bootstrap", default=True, description="Enable 3-stage bootstrap")
-    variant(
-        "graphite", default=False, description="Enable Graphite loop optimizations (requires ISL)"
-    )
-    variant(
-        "build_type",
-        default="RelWithDebInfo",
-        values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel"),
-        description="CMake-like build type. "
-        "Debug: -O0 -g; Release: -O3; "
-        "RelWithDebInfo: -O2 -g; MinSizeRel: -Os",
-    )
-    variant(
-        "profiled", default=False, description="Use Profile Guided Optimization", when="+bootstrap"
-    )
-    # https://gcc.gnu.org/install/prerequisites.html
-    # mawk is not sufficient for go support
-    # dependencies required for git versions
-    # GCC 7.3 does not compile with newer releases on some platforms, see
-    patch(
-        "https://github.com/gcc-mirror/gcc/commit/ea2798892de373b14f9fc7ae8a0d820eaddca98c.patch?full_index=1",
-        sha256="0999dbf856725566373f25a6f192a3520ea036db8e1f31928aae9750e6e38be7",
-        when="@15:15.2",
-    )
-    if sys.platform == "darwin":
-        # Fix parallel build on APFS filesystem
-        # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81797
-        if macos_version() >= Version("10.13"):
-            # from homebrew via macports
-            # https://trac.macports.org/ticket/56502#no1
-            # see also: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=83531
-            patch("darwin/headers-10.13-fix.patch", when="@5.5.0")
-        if macos_version() >= Version("10.14"):
-            # Fix system headers for Mojave SDK:
-            # https://github.com/Homebrew/homebrew-core/pull/39041
-            patch(
-                "https://raw.githubusercontent.com/Homebrew/formula-patches/b8b8e65e/gcc/8.3.0-xcode-bug-_Atomic-fix.patch",
-                sha256="33ee92bf678586357ee8ab9d2faddf807e671ad37b97afdd102d5d153d03ca84",
-                when="@6:8.3",
             )
             # See https://raw.githubusercontent.com/Homebrew/homebrew-core/3b7db4457ac64a31e3bbffc54b04c4bd824a4a4a/Formula/gcc.rb

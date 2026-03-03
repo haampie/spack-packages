ad657e75cfc2b43c01cc97393bc5c0de5c88a3a3
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:43:49 +0100

    reduce: delete 2 files and 7 calls

--- repos/spack_repo/builtin/build_systems/oneapi.py
+++ repos/spack_repo/builtin/build_systems/oneapi.py
@@ -43,7 +43,6 @@ class IntelOneApiPackage(Package):
     ]:
         conflicts(c, msg="This package in only available for x86_64 and Linux")
     # Add variant to toggle environment modifications from vars.sh
-    variant("envmods", default=True, description="Toggles environment modifications")
     @staticmethod
     def update_description(cls):
         """Updates oneapi package descriptions with common text."""

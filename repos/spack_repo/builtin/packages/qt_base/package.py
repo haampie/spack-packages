1015ec73a3f595fa287e8045dc2f360b32ed92c7
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:51:05 +0100

    reduce: 28 lines across 14 regions

--- repos/spack_repo/builtin/packages/qt_base/package.py
+++ repos/spack_repo/builtin/packages/qt_base/package.py
@@ -4,23 +4,12 @@ from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 from spack.package import *
 MACOS_VERSION = macos_version() if sys.platform == "darwin" else None
 class QtPackage(CMakePackage):
-    """Base package for Qt6 components"""
-    homepage = "https://www.qt.io"
-    @staticmethod
     def get_url(qualname):
         _url = "https://github.com/qt/{}/archive/refs/tags/v6.2.3.tar.gz"
-        return _url.format(qualname.lower())
-    @staticmethod
     def get_git(qualname):
         _git = "https://github.com/qt/{}.git"
-        return _git.format(qualname.lower())
-    @staticmethod
     def get_list_url(qualname):
         _list_url = "https://github.com/qt/{}/tags"
-        return _list_url.format(qualname.lower())
-    # Default dependencies for all qt-* components
-    # List of unnecessary directories in src/3rdparty
-    vendor_deps_to_remove = []
 class QtBase(QtPackage):
     variant("gui", default=True, description="Build the Qt GUI module and dependencies.")
     variant("shared", default=True, description="Build shared libraries.")

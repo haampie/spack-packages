dc15def2c144dc46e16681f2f159d4bb7cb7c48f
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:51:13 +0100

    reduce: 18 lines across 9 regions

--- repos/spack_repo/builtin/build_systems/compiler.py
+++ repos/spack_repo/builtin/build_systems/compiler.py
@@ -23,14 +23,10 @@ class CompilerPackage(PackageBase):
     #: Flag used to get verbose output
     verbose_flags: str = "-v"
     #: Flag to activate OpenMP support
-    openmp_flag: str = "-fopenmp"
-    implicit_rpath_libs: List[str] = []
     @property
     def cc(self) -> Optional[str]:
         assert self.spec.concrete, "cannot retrieve C compiler, spec is not concrete"
         if self.spec.external:
             return self.spec.extra_attributes.get("compilers", {}).get("c", None)
         return self._cc_path()
-    def _cc_path(self) -> Optional[str]:
-        """Returns the path to the C compiler, if the package was installed by Spack"""
         return None
--- repos/spack_repo/builtin/packages/gcc/package.py
+++ repos/spack_repo/builtin/packages/gcc/package.py
@@ -4,24 +4,12 @@ from spack_repo.builtin.build_systems.gnu import GNUMirrorPackage
 from spack.package import *
 class Gcc(AutotoolsPackage, GNUMirrorPackage, CompilerPackage):
     list_url = "https://ftp.gnu.org/gnu/gcc/"
-    list_depth = 1
-    keep_werror = "all"
     provides("c", "cxx", when="languages=c,c++")
-    provides("c", when="languages=c")
-    version("master", branch="master")
-    # Latest stable
     version("15.2.0", sha256="438fd996826b0c82485a29da03a72d71d6e3541a83ec702df4271f6fe025d24e")
-    # Previous stable series releases
-    # Final releases of previous versions
-    # Deprecated older non-final releases
-    with default_args(deprecated=True):
-        version("4.8.4", sha256="4a80aa23798b8e9b5793494b8c976b39b8d9aa2e53cd5ed5534aff662a7f8695")
     variant(
         "languages",
         default="c,c++,fortran",
         values=(
-            "ada",
-            "brig",
             "c",
             "c++",
         ),

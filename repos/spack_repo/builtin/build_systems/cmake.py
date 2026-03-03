046f99d088b9adda88448c3381268e9c4badeb6b
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:44:19 +0100

    reduce: 48 lines across 1 regions

--- repos/spack_repo/builtin/build_systems/cmake.py
+++ repos/spack_repo/builtin/build_systems/cmake.py
@@ -148,53 +148,5 @@ class CMakePackage(PackageBase):
         # CMake has libs options separated by language. Apply ours to each.
         if flags["ldlibs"]:
             libs_flags = " ".join(flags["ldlibs"])
-            libs_string = "-DCMAKE_{0}_STANDARD_LIBRARIES={1}"
-            for lang in langs:
-                self.cmake_flag_args.append(libs_string.format(lang, libs_flags))
-    # Legacy methods (used by too many packages to change them,
-    # need to forward to the builder)
-    def define(self, cmake_var: str, value: Any) -> str:
-        return define(cmake_var, value)
-    def define_from_variant(self, cmake_var: str, variant: Optional[str] = None) -> str:
-        return define_from_variant(self, cmake_var, variant)
-@register_builder("cmake")
-class CMakeBuilder(BuilderWithDefaults):
-    """The cmake builder encodes the default way of building software with CMake. IT
-    has three phases that can be overridden:
-        1. :py:meth:`~.CMakeBuilder.cmake`
-        2. :py:meth:`~.CMakeBuilder.build`
-        3. :py:meth:`~.CMakeBuilder.install`
-    They all have sensible defaults and for many packages the only thing
-    necessary will be to override :py:meth:`~.CMakeBuilder.cmake_args`.
-    For a finer tuning you may also override:
-        +-----------------------------------------------+--------------------+
-        | **Method**                                    | **Purpose**        |
-        +===============================================+====================+
-        | :py:meth:`~.CMakeBuilder.root_cmakelists_dir` | Location of the    |
-        |                                               | root CMakeLists.txt|
-        +-----------------------------------------------+--------------------+
-        | :py:meth:`~.CMakeBuilder.build_directory`     | Directory where to |
-        |                                               | build the package  |
-        +-----------------------------------------------+--------------------+
-    """
-    #: Phases of a CMake package
-    phases: Tuple[str, ...] = ("cmake", "build", "install")
-    #: Names associated with package methods in the old build-system format
-    package_methods: Tuple[str, ...] = ("cmake_args", "check")
-    #: Names associated with package attributes in the old build-system format
-    package_attributes: Tuple[str, ...] = (
-        "build_targets",
-        "install_targets",
-        "build_time_test_callbacks",
-        "archive_files",
-        "root_cmakelists_dir",
-        "std_cmake_args",
-        "build_dirname",
-        "build_directory",
-    )
-    #: Targets to be used during the build phase
-    build_targets: List[str] = []
-    #: Targets to be used during the install phase
-    install_targets = ["install"]
     #: Callback names for build-time test
     build_time_test_callbacks = ["check"]

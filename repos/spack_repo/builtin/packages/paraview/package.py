6bec775143ac273a94d16af9355404c881f6462c
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:48:05 +0100

    reduce: 13 lines across 1 regions

--- repos/spack_repo/builtin/packages/paraview/package.py
+++ repos/spack_repo/builtin/packages/paraview/package.py
@@ -11,19 +11,6 @@ from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
 from spack.package import *
 IS_WINDOWS = sys.platform == "win32"
-# This is (more or less) the mapping hard-coded in VTK-m logic
-# see https://gitlab.kitware.com/vtk/vtk-m/-/blob/v2.1.0/CMake/VTKmDeviceAdapters.cmake?ref_type=tags#L221-247
-supported_cuda_archs = {
-    "20": "fermi",
-    "21": "fermi",
-    "30": "kepler",
-    "32": "kepler",
-    "75": "turing",
-    "80": "ampere",
-    "86": "ampere",
-}
-# This is a list of paraview variants that require the viskores library.
-viskores_dependency_variants = ["+cuda", "+fides", "+rocm"]
 class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     git = "https://gitlab.kitware.com/paraview/paraview.git"
     tags = ["e4s"]

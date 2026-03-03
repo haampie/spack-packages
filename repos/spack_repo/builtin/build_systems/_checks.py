d22100e9b0587a4f369b8d3f881566da7ae4f215
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:55:17 +0100

    reduce: 11 lines across 1 regions

--- repos/spack_repo/builtin/build_systems/_checks.py
+++ repos/spack_repo/builtin/build_systems/_checks.py
@@ -1,14 +1,3 @@
-from typing import List
-from spack.package import (
-    Builder,
-    Spec,
-)
-def ensure_build_dependencies_or_raise(spec: Spec, dependencies: List[str], error_msg: str):
-    msg = (
-    )
-    for dep in missing_deps:
-        msg += '    depends_on("{0}", type="build", when="@{1} {2}")\n'.format(
-        )
 def execute_build_time_tests(builder: Builder):
     """Execute the build-time tests prescribed by builder.
     """

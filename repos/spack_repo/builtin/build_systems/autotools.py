e41868c7a9f527b24fa523b086d2b426bbb6ab2f
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:56:14 +0100

    reduce: 4 lines across 2 regions

--- repos/spack_repo/builtin/build_systems/autotools.py
+++ repos/spack_repo/builtin/build_systems/autotools.py
@@ -2,9 +2,6 @@ from spack.package import (
     PackageBase,
     build_system,
     depends_on,
-    execute_install_time_tests,
-    when,
-    working_dir,
 )
 class AutotoolsPackage(PackageBase):
         depends_on("gmake", type="build")

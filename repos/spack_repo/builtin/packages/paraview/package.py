21f4a23dff681979e09799998267f128bbab3eca
Harmen Stoppels <me@harmenstoppels.nl>
2026-03-03 10:42:13 +0100

    reduce: 58 lines across 1 regions

--- repos/spack_repo/builtin/packages/paraview/package.py
+++ repos/spack_repo/builtin/packages/paraview/package.py
@@ -55,64 +55,6 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     variant("examples", default=False, description="Build examples")
     variant("hdf5", default=False, description="Use external HDF5")
     variant("shared", default=True, description="Builds a shared version of the library")
-    variant("kits", default=True, description="Use module kits")
-    variant("adios2", default=False, description="Enable ADIOS2 support", when="@5.8:")
-    variant("fides", default=False, description="Enable Fides support", when="@5.9:")
-    variant("visitbridge", default=False, description="Enable VisItBridge support")
-    # Legacy rendering dropped in 5.5
-    # See commit: https://gitlab.kitware.com/paraview/paraview/-/commit/798d328c
-    # in 5.7 you cannot reduce the size of the code for Catalyst builds.
-    # before 5.3.0, ParaView didn't have VTK-m/Viskores
-    # paraview@5.9.0 is recommended when using the xl compiler
-    # See https://gitlab.kitware.com/paraview/paraview/-/merge_requests/4433
-    # VTK < 8.2.1 can't handle Python 3.8
-    # This affects Paraview <= 5.7 (VTK 8.2.0)
-    # https://gitlab.kitware.com/vtk/vtk/-/issues/17670
-    # openPMD is implemented as a Python module and provides ADIOS2 and HDF5 backends
-    # Handle X11 dependencies
-    # X is only used on Unix like platforms
-    # When on linux, X is required for Qt
-    for plat in ["linux", "freebsd"]:
-        with when(f"platform={plat}"):
-            requires("+x", when="+qt", msg="Qt support requires GLX on Linux/FreeBSD")
-    with when("+x"):
-        # When Qt and X are enabled, GLX is required in the runtime
-        requires("^[virtuals=gl] glx", when="@:5")
-    # ParaView@:5 support Qt5 and requires a GL provider to be known at
-    # build/link time.
-    with when("@:5"):
-        with when("+qt"):
-            # https://discourse.paraview.org/t/paraview-5-9-and-minimum-recommended-qt-version/5333
-            # Headless rendering not supported with Qt
-            conflicts("osmesa")
-            conflicts("egl")
-        # CUDA ARCH
-        # VTK-m and transitively ParaView does not support Tesla Arch
-        for _arch in ("10", "11", "12", "13"):
-            conflicts(f"cuda_arch={_arch}", when="+cuda", msg="ParaView requires cuda_arch >= 20")
-        # Starting from cmake@3.18, CUDA architecture managament can be delegated to CMake.
-        # Hence, it is possible to rely on it instead of relying on custom logic updates from
-        # VTK-m for newer architectures (wrt mapping).
-        pattern = re.compile(r"\d+")
-        for _arch in CudaPackage.cuda_arch_values:
-            _number = re.match(pattern, _arch).group()
-            if int(_number) > 86:
-                conflicts("cmake@:3.17", when=f"cuda_arch={_arch}")
-        # We only support one single Architecture
-        for _arch, _other_arch in itertools.permutations(CudaPackage.cuda_arch_values, 2):
-            conflicts(
-                "cuda_arch={0}".format(_arch),
-                when="cuda_arch={0}".format(_other_arch),
-                msg="Paraview only accepts one architecture value",
-            )
-        # Dependencies for vendored VTK-m
-        # CUDA thrust is already include in the CUDA pkg
-        depends_on("rocthrust", when="@5.13: +rocm ^cmake@3.24:")
-        for target in ROCmPackage.amdgpu_targets:
-            depends_on(
-                "kokkos@:3.7 +rocm amdgpu_target={0}".format(target),
-                when="+rocm amdgpu_target={0}".format(target),
-            )
     with when("@6:"):
         # ParaView 6 and later will not support Spack builds with Qt5.
         with when("+qt"):

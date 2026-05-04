eb46fd486d9d44cbb1f70f3ee3019fc944162b40
Harmen Stoppels <me@harmenstoppels.nl>
2026-05-04 12:25:47 +0200

    clingo-bootstrap-pgo: single-threaded to make reproducible
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- repos/spack_repo/builtin/packages/clingo_bootstrap_pgo/package.py
+++ repos/spack_repo/builtin/packages/clingo_bootstrap_pgo/package.py
@@ -15,6 +15,7 @@ class ClingoBootstrapPgo(Package):
 
     maintainers("haampie")
 
+    version("1.0.2", commit="30bfa8a73fdf1d797c1d1d634aedfa48196c7aa8")
     version("1.0.1", commit="4326d65113f05b9030c36bf2886dbb0873013be3")
     version("1.0.0", commit="64bec625ae06b32b7f5f01bccf9d27d0432a018f")
 
